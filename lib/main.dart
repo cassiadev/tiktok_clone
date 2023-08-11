fcd76d2de6e1a12ff61058a3238956cdeba34a8b
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-08-11 22:23:26 +0900

    Removed redundant comment
    Removed the comment which was just there when the app was first built automatically with flutter create command.

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,7 @@ void main() {
 
 class TikTokApp extends StatelessWidget {
   const TikTokApp({super.key});
-
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
