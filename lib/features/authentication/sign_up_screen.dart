e7740875e7c8f1e56fcde19f783aad3e159c5879
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-09-12 21:56:53 +0900

    Added screens and common ThemeData
    ・Added EmailScreen and UsernameScreen
    ・Added ThemeData elements in main.dart to be applied commonly to every screen
    ・Slight refactoring

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/features/authentication/email_screen.dart';
 import 'package:tiktok_clone/features/authentication/login_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
@@ -9,11 +10,19 @@ import '../../constants/sizes.dart';
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
-  void onLoginTap(BuildContext context) {
+  void _onLoginTap(BuildContext context) {
     Navigator.of(context).push( // push() stacks new screen on the old one
       MaterialPageRoute(
         builder: (context) => const LoginScreen(),
       )
+    );
+  }
+
+  void _onEmailTap(BuildContext context) {
+    Navigator.of(context).push( // push() stacks new screen on the old one
+        MaterialPageRoute(
+          builder: (context) => const EmailScreen(),
+        )
     );
   }
 
@@ -25,15 +34,15 @@ class SignUpScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(
               horizontal: Sizes.size40),
           child: Column(
-            children: const [
+            children: [
               Gaps.v80,
-              Text('Sign up for TikTok',
+              const Text('Sign up for TikTok',
                 style: TextStyle(
                   fontSize: Sizes.size24,
                   fontWeight: FontWeight.w700,
                 ),),
               Gaps.v20,
-              Text('Create a profile, follow other accounts, make your own videos, and more.',
+              const Text('Create a profile, follow other accounts, make your own videos, and more.',
                 style: TextStyle(
                   fontSize: Sizes.size16,
                   color: Colors.black45,
@@ -41,12 +50,15 @@ class SignUpScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               Gaps.v40,
-              AuthButton(
-                  icon: FaIcon(FontAwesomeIcons.user),
-                  text: 'Use email and password',
+              GestureDetector(
+                onTap: () => _onEmailTap(context),
+                child: const AuthButton(
+                    icon: FaIcon(FontAwesomeIcons.user),
+                    text: 'Use email and password',
+                ),
               ),
               Gaps.v16,
-              AuthButton(
+              const AuthButton(
                   icon: FaIcon(FontAwesomeIcons.apple),
                   text: 'Continue with Apple',
               ),
@@ -55,7 +67,7 @@ class SignUpScreen extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.grey.shade100,
+        color: Colors.grey.shade50,
         elevation: 2,
         child: Padding(
           padding: const EdgeInsets.symmetric(vertical: Sizes.size32),
@@ -65,7 +77,7 @@ class SignUpScreen extends StatelessWidget {
               const Text('Already have an account?'),
               Gaps.h5,
               GestureDetector(
-                onTap: () => onLoginTap(context),
+                onTap: () => _onLoginTap(context),
                 child: Text('Log in',
                   style: TextStyle(
                     color: Theme.of(context).primaryColor,
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 
+import 'constants/sizes.dart';
+
 void main() {
   runApp(const TikTokApp());
 }
@@ -14,6 +16,17 @@ class TikTokApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
       home: const SignUpScreen(),
     );
