1b20a565e03855a1cff1fc3b0e00d605f1b88c99
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-09-18 19:26:56 +0900

    Created BirthdayScreen

--- lib/features/authentication/birthday_screen.dart
+++ lib/features/authentication/birthday_screen.dart
@@ -1,10 +1,109 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-class BirthdayScreen extends StatelessWidget {
+import 'email_screen.dart';
+
+class BirthdayScreen extends StatefulWidget {
   const BirthdayScreen({super.key});
 
   @override
+  State<BirthdayScreen> createState() => _BirthdayScreenState();
+}
+
+class _BirthdayScreenState extends State<BirthdayScreen> {
+  final TextEditingController _birthdayController = TextEditingController();
+
+  DateTime initialDate = DateTime.now();
+
+  @override
+  void initState() {
+    super.initState();
+    _setTextFieldDate(initialDate);
+  }
+
+  @override
+  void dispose() {
+    _birthdayController.dispose(); // Deletes event listeners regarding _birthdayController so that memory leak does not happen
+    super.dispose(); // Since initState() starts with super.initState(), when disposing super.dispose() should be at last
+  }
+
+  void _onNextTap() {
+    Navigator.of(context).push( // Since it is inside the State in a StatefulWidget, there is no need for _onNextTap to have a BuildContext parameter
+      MaterialPageRoute(builder: (context) => const EmailScreen()),
+    );
+  }
+
+  void _setTextFieldDate(DateTime date) {
+    final textDate = date.toString().split(' ').first;
+    _birthdayController.value = TextEditingValue(text: textDate);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign up',),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Gaps.v40,
+            const Text('When is your birthday?',
+              style: TextStyle(
+                fontSize: Sizes.size24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Gaps.v8,
+            const Text('Your birthday won\'t be shown publicly.',
+              style: TextStyle(
+                fontSize: Sizes.size16,
+                color: Colors.black54,
+              ),
+            ),
+            Gaps.v16,
+            TextField(
+              enabled: false,
+              controller: _birthdayController,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey.shade400,
+                  ),
+                ),
+              ),
+              cursorColor: Theme.of(context).primaryColor,
+            ),
+            Gaps.v16,
+            GestureDetector(
+              onTap: _onNextTap, // Since it is inside the State in a StatefulWidget, there is no need for _onNextTap to have a BuildContext parameter
+              child: const FormButton(disabled: false),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 300,
+          child: CupertinoDatePicker(
+            mode: CupertinoDatePickerMode.date,
+            initialDateTime: initialDate,
+            maximumDate: initialDate,
+            onDateTimeChanged: _setTextFieldDate,
+          ),
+        ),
+      ),
+    );
   }
 }
+
