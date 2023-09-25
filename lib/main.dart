f5456124fc775bb55f9eba01c2f1cd1d3fa9b401
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-09-25 11:25:42 +0900

    Created NavTab widget and applied it to MainNavigationScreen
    The custom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
@@ -14,22 +13,22 @@ class TikTokApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      title: 'Flutter Demo',
-      // theme: ThemeData(
-      //   primaryColor: const Color(0xFFE9435A),
-      //   scaffoldBackgroundColor: Colors.white,
-      //   appBarTheme: const AppBarTheme(
-      //     foregroundColor: Colors.black,
-      //     backgroundColor: Colors.white,
-      //     elevation: 0,
-      //     titleTextStyle: TextStyle(
-      //       color: Colors.black,
-      //       fontSize: Sizes.size16 + Sizes.size2,
-      //       fontWeight: FontWeight.w600,
-      //     ),
-      //   ),
-      // ),
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
       home: const MainNavigationScreen(),
     );
   }
