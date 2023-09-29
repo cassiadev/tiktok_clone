6101676f3e368a02af8be268abb6d60f17527ebb
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-09-29 11:32:11 +0900

    Created VideoTimelineScreen
    ・Applied PageView.builder
    ・Deleted the temporary stateful screen

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
-import 'package:tiktok_clone/features/main_navigation/stf_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
+import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
 
 import '../../constants/sizes.dart';
 
@@ -43,19 +43,19 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
         children: [
           Offstage( // Creates a widget that visually hides its child
             offstage: _selectedIndex != 0,
-            child: const StfScreen(),
+            child: VideoTimelineScreen(),
           ),
           Offstage(
             offstage: _selectedIndex != 1,
-            child: const StfScreen(),
+            child: VideoTimelineScreen(),
           ),
           Offstage(
             offstage: _selectedIndex != 3,
-            child: const StfScreen(),
+            child: VideoTimelineScreen(),
           ),
           Offstage(
             offstage: _selectedIndex != 4,
-            child: const StfScreen(),
+            child: VideoTimelineScreen(),
           ),
         ],
       ),
---
+++ lib/features/videos/video_timeline_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class VideoTimelineScreen extends StatefulWidget {
+  const VideoTimelineScreen({super.key});
+
+  @override
+  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+}
+
+class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  int _itemCount = 4;
+
+  List<Color> colors = [
+    Colors.blue,
+    Colors.red,
+    Colors.yellow,
+    Colors.teal,
+  ];
+
+  void _onPageChanged(int pageNum) {
+    if (pageNum == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      colors.addAll([
+        Colors.blueAccent,
+        Colors.pink,
+        Colors.amber,
+        Colors.green,
+      ]);
+      setState(() {});
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      pageSnapping: true, // If pageSnapping is set false, the sticky page snap does not happen as moving the page. Default is true
+      scrollDirection: Axis.vertical,
+      onPageChanged: _onPageChanged,
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => Container( // itemBuilder of PageView.builder is similar to or could be same as ListView.builder, as it takes context and index as arguments
+        color: colors.elementAt(index),
+        child: Center(
+          child: Text("Screen $index",
+            style: const TextStyle(
+              fontSize: 68,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
