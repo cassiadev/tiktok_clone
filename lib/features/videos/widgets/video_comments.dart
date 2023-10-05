e719ed829aba9a98369f2a79dfcaf101e001e033
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-10-05 22:00:05 +0900

    Updated VideoComments
    ・Enabled textInputActions to return new lines
    ・Enabled dismissing of adding-comment part with tapping the Stack body
    ・Created comment submitting button but for now temporarily input dismissing keyboard function

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -11,9 +11,23 @@ class VideoComments extends StatefulWidget {
 }
 
 class _VideoCommentsState extends State<VideoComments> {
+  bool _isWritingComment = false;
 
   void _onClosePressed() {
     Navigator.of(context).pop();
+  }
+
+  void _dismissKeyboard() {
+    FocusScope.of(context).unfocus();
+    setState(() {
+      _isWritingComment = false;
+    });
+  }
+
+  void _onStartWritingComment() {
+    setState(() {
+      _isWritingComment = true;
+    });
   }
 
   @override
@@ -38,103 +52,142 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: Stack(
-          children: [
-            ListView.separated(
-              padding: const EdgeInsets.symmetric(
-                vertical: Sizes.size10,
-                horizontal: Sizes.size16,
-              ),
-              separatorBuilder: (context, index) => Gaps.v20,
-              itemCount: 10,
-              itemBuilder: (context, index) => Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const CircleAvatar(
-                    radius: 18,
-                    child: Text('YouYourYouYours'),
-                  ),
-                  Gaps.h10,
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        body: GestureDetector(
+          onTap: _dismissKeyboard,
+          child: Stack(
+            children: [
+              ListView.separated(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size10,
+                  horizontal: Sizes.size16,
+                ),
+                separatorBuilder: (context, index) => Gaps.v20,
+                itemCount: 10,
+                itemBuilder: (context, index) => Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CircleAvatar(
+                      radius: 18,
+                      child: Text('YouYourYouYours'),
+                    ),
+                    Gaps.h10,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('YouYourYouYours',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size14,
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                          Gaps.v3,
+                          const Text('That\'s not it I\'ve seen the same thing but also in a cave'),
+                        ],
+                      ),
+                    ),
+                    Gaps.h10,
+                    Column(
                       children: [
-                        Text('YouYourYouYours',
+                        FaIcon(FontAwesomeIcons.heart,
+                          size: Sizes.size24,
+                          color: Colors.grey.shade500,
+                        ),
+                        Gaps.v2,
+                        Text('52.1K',
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: Sizes.size14,
                             color: Colors.grey.shade500,
                           ),
                         ),
-                        Gaps.v3,
-                        const Text('That\'s not it I\'ve seen the same thing but also in a cave'),
                       ],
                     ),
-                  ),
-                  Gaps.h10,
-                  Column(
-                    children: [
-                      FaIcon(FontAwesomeIcons.heart,
-                        size: Sizes.size24,
-                        color: Colors.grey.shade500,
-                      ),
-                      Gaps.v2,
-                      Text('52.1K',
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Positioned( // bottomNavigationBar cannot bring up the BottomAppBar for the comment
-              bottom: 0,
-              width: mediaQuerySize.width,
-              child: BottomAppBar(
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: Sizes.size16,
-                    vertical: Sizes.size10, // Padding for the entire Row has limits, so if you want to modify the size of the TextField precisely, directly surround the TextField with SizedBox
-                  ),
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 18,
-                        backgroundColor: Colors.grey.shade500,
-                        foregroundColor: Colors.white,
-                        child: const Text('YouYourYouYours'),
-                      ),
-                      Gaps.h10,
-                      Expanded(
-                        child: SizedBox(
-                          height: Sizes.size44, // Directly surround the TextField with SizedBox and modify the height
-                          child: TextField(
-                            cursorColor: Theme.of(context).primaryColor,
-                            decoration: InputDecoration(
-                              hintText: 'Add comment...',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(Sizes.size12),
-                                borderSide: BorderSide.none,
-                              ),
-                              filled: true,
-                              fillColor: Colors.grey.shade200,
-                              contentPadding: const EdgeInsets.symmetric(
-                                horizontal: Sizes.size12,
-                                vertical: Sizes.size10,
+              Positioned( // bottomNavigationBar cannot bring up the BottomAppBar for the comment
+                bottom: 0,
+                width: mediaQuerySize.width,
+                child: BottomAppBar(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size16,
+                      vertical: Sizes.size10, // Padding for the entire Row has limits, so if you want to modify the size of the TextField precisely, directly surround the TextField with SizedBox
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 18,
+                          backgroundColor: Colors.grey.shade500,
+                          foregroundColor: Colors.white,
+                          child: const Text('YouYourYouYours'),
+                        ),
+                        Gaps.h10,
+                        Expanded(
+                          child: SizedBox(
+                            height: Sizes.size44, // Directly surround the TextField with SizedBox and modify the height
+                            child: TextField(
+                              onTap: _onStartWritingComment,
+                              textInputAction: TextInputAction.newline,
+                              expands: true, // TextInputAction.newline is enabled with expands, minLines and maxLines
+                              minLines: null,
+                              maxLines: null,
+                              cursorColor: Theme.of(context).primaryColor,
+                              decoration: InputDecoration(
+                                hintText: 'Add comment...',
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(Sizes.size12),
+                                  borderSide: BorderSide.none,
+                                ),
+                                filled: true,
+                                fillColor: Colors.grey.shade200,
+                                contentPadding: const EdgeInsets.symmetric(
+                                  horizontal: Sizes.size12,
+                                  vertical: Sizes.size10,
+                                ),
+                                suffixIcon: Padding(
+                                  padding: const EdgeInsets.only(right: Sizes.size14),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min, // Similar to wrap_content, makes the suffix icons move to the right end
+                                    children: [
+                                      FaIcon(
+                                        FontAwesomeIcons.at,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      FaIcon(
+                                        FontAwesomeIcons.gift,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      FaIcon(
+                                        FontAwesomeIcons.faceSmile,
+                                        color: Colors.grey.shade900,
+                                      ),
+                                      Gaps.h10,
+                                      if (_isWritingComment)
+                                        GestureDetector(
+                                          onTap: _dismissKeyboard, // Temporarily input _dismissKeyboard before applying the actual registration of the comment
+                                          child: FaIcon(
+                                            FontAwesomeIcons.circleArrowUp,
+                                            color: Theme.of(context).primaryColor,
+                                          ),
+                                        ),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
