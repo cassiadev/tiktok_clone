df835fc32e02b6e0fb3608b0fe166d34638adc98
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-10-05 12:01:49 +0900

    Created content of VideoComments
    Applied ListView.separated

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -35,11 +36,66 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
-          padding: const EdgeInsets.symmetric(vertical: Sizes.size10, horizontal: Sizes.size16),
+        body: ListView.separated(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size10,
+            horizontal: Sizes.size16,
+          ),
+          separatorBuilder: (context, index) => Gaps.v20,
           itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            child: const Text('Voila c\'est le commentaire'),
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                child: Text('YouYourYouYours'),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('YouYourYouYours',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size14,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                    Gaps.v3,
+                    const Text('That\'s not it I\'ve seen the same thing but also in a cave'),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(FontAwesomeIcons.heart,
+                    size: Sizes.size24,
+                    color: Colors.grey.shade500,
+                  ),
+                  Gaps.v2,
+                  Text('52.1K',
+                    style: TextStyle(
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                child: Text('YouYourYouYours'),
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+              ),
+            ],
           ),
         ),
       ),
