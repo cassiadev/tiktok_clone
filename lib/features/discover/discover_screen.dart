94a7923174189b776ef5bbe650aeaae3ed54f048
cassiadev <37288667+cassiadev@users.noreply.github.com>
2023-10-09 15:59:27 +0900

    Created DiscoverScreen
    ・Temporarily changed first tab to be shown to DiscoverScreen
    ・Applied PreferredSizeWidget using bottom in TabBar
    ・Applied NoSplash.splashFactory to erase the splash effect

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = [
+  'Top',
+  'Users',
+  'Videos',
+  'Sounds',
+  'lIVE',
+  'Shopping',
+  'Brands',
+];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Discover',
-
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Discover'),
+          elevation: 1, // Works as an underline for the appBar
+          bottom: TabBar( // bottom has the type of PreferredSizeWidget, wishes to be at a certain size but does not constrain the size of its children, which usually comes as a TabBar
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+            ),
+            isScrollable: true,
+            splashFactory: NoSplash.splashFactory, // Erases the splash effect when a tab is clicked
+            labelColor: Colors.black,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size16,
+            ),
+            indicatorColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            tabs: [for (var tab in tabs)
+              Tab(
+                text: tab,
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: const TextStyle(
+                    fontSize: Sizes.size28,
+                  ),
+                ),
+            ),
+          ],
         ),
       ),
     );
